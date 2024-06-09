67a4dea7d6c9b5a4fad8580e45125be112a13667
Michael Wilder <mikewilder01@gmail.com>
2024-06-09 00:57:04 -0400

    randomized resting animations, started adding pause time between loops. tomorrow need to figure out ow to make eightfold movement a generic function i can use for all future critters

--- core/src/objects/animals/bison/Bison.java
+++ core/src/objects/animals/bison/Bison.java
@@ -1,6 +1,5 @@
 package objects.animals.bison;
 
-
 import com.badlogic.gdx.graphics.g2d.*;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
@@ -9,34 +8,45 @@ import com.mygdx.eightfold.screens.GameScreen;
 import objects.GameAssets;
 import objects.animals.object_helper.BisonManager;
 import objects.player.GameEntity;
-import  helper.movement.Facing;
+import helper.movement.Facing;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 import static helper.Constants.FRAME_DURATION;
 import static helper.Constants.PPM;
-
-// Import the Direction enum
-
 
 public class Bison extends GameEntity {
     private Map<String, Animation<TextureRegion>> animations;
     private Animation<TextureRegion> currentAnimation;
     private float stateTime;
+    private float randomStateTime;
     private Facing facingDirection;
     private int id;
     private Sprite sprite;
     private GameAssets gameAssets;
     private float movementThreshold;
     private boolean isResting;
+    private Random random;
+   ;
+    private boolean isPaused;
+    private float restingTime;
+    private float pauseDuration;
+
     public Bison(float width, float height, float x, float y, Body body, Facing initialDirection, GameScreen gameScreen, int bisonId, GameAssets gameAssets) {
         super(width, height, body, gameScreen, gameAssets);
+        this.random = new Random();
         this.stateTime = 0f;
+        this.randomStateTime = random.nextFloat();
         this.id = bisonId;
         this.facingDirection = initialDirection;
         this.body = body;
         this.gameAssets = gameAssets;
         this.movementThreshold = 1f;
+        this.restingTime = 0f;
+        this.pauseDuration = 3f; // 3 seconds pause duration
+        this.isPaused = true;
+
 
         // Load animations
         loadAnimations();
@@ -49,7 +59,7 @@ public class Bison extends GameEntity {
     @Override
     public void update(float delta) {
         stateTime += delta; // Update the state time
-
+        randomStateTime += delta / 2;
         // Update sprite position
         float x = body.getPosition().x * PPM;
         float y = body.getPosition().y * PPM;
@@ -58,14 +68,26 @@ public class Bison extends GameEntity {
         // Set the origin of the sprite to its center
         sprite.setOriginCenter();
 
-        // Stop the bison if the velocity is below a threshold
-        Vector2 linearVelocity = body.getLinearVelocity();
-        if (linearVelocity.len() < 0.8) {
-            body.setLinearVelocity(0, 0);
+        if (!isPaused) {
+            // Determine the current animation frame and update sprite size if needed
+            updateAnimation(body.getLinearVelocity());
+        } else {
+            // If paused, increment resting time
+            restingTime += delta;
+            // Check if pause duration has passed
+            if (restingTime >= pauseDuration) {
+                // Reset resting time
+                restingTime = 0f;
+                // Resume animation
+                isPaused = false;
+            } else {
+                System.out.println("resting time: " + restingTime);
+                // If still in pause duration, display the first frame of the animation
+                TextureRegion frame = currentAnimation.getKeyFrame(0);
+                sprite.setRegion(frame);
+                sprite.setSize(frame.getRegionWidth(), frame.getRegionHeight());
+            }
         }
-
-        // Determine the current animation frame and update sprite size if needed
-        updateAnimation(linearVelocity);
     }
 
 
@@ -104,8 +126,6 @@ public class Bison extends GameEntity {
         animations.put("stationaryHorizontal", createAnimation("Bison_Horizontal_Rest", 5, "animals/bison/walking/atlases/eightfold/bison-horizontal.atlas"));
         animations.put("stationaryUpDiagonal", createAnimation("Bison_DiagUP_Rest", 5, "animals/bison/walking/atlases/eightfold/bison-diagonal.atlas"));
         animations.put("stationaryDownDiagonal", createAnimation("Bison_DiagDOWN_Rest", 5, "animals/bison/walking/atlases/eightfold/bison-diagonal.atlas"));
-        //animations.put("stationaryDownRight", createAnimation("Bison_DownRight_Rest", 5, "animals/bison/walking/atlases/eightfold/bison-diagonal.atlas"));
-        //animations.put("stationaryDownLeft", createAnimation("Bison_DownLeft_Rest", 5, "animals/bison/walking/atlases/eightfold/bison-diagonal.atlas"));
         currentAnimation = animations.get("stationaryHorizontal"); // Default to stationary animation
     }
 
@@ -140,10 +160,18 @@ public class Bison extends GameEntity {
             setRestingAnimation();
         }
 
-        TextureRegion frame = currentAnimation.getKeyFrame(stateTime, true);
-        sprite.setRegion(frame);
+        TextureRegion frame;
+        if (isResting) {
 
-        // Adjust sprite size and flip based on direction
+            // Limit random state time to animation duration
+
+
+            frame = currentAnimation.getKeyFrame(randomStateTime, true);
+        } else {
+            frame = currentAnimation.getKeyFrame(stateTime, true);
+        }
+
+        sprite.setRegion(frame);
         sprite.setSize(frame.getRegionWidth(), frame.getRegionHeight());
         sprite.setOriginCenter(); // Ensure the origin is centered
         updateSpriteFlip();
@@ -193,7 +221,6 @@ public class Bison extends GameEntity {
         }
         sprite.setOriginCenter(); // Set the origin to the center of the sprite
     }
-
 
     private void setWalkingAnimation(float vx, float vy) {
         isResting = false;
