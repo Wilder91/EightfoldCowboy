f00dae7499bca43d0fffa84f378c405c760e909a
Mike Wilder <65726732+Wilder91@users.noreply.github.com>
2025-04-08 21:05:02 -0400

    merging from eightbit branch

---
+++ core/src/com/mygdx/eightfold/player/GameEntity.java
@@ -0,0 +1,33 @@
+package com.mygdx.eightfold.player;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.mygdx.eightfold.screens.GameScreen;
+import com.mygdx.eightfold.GameAssets;
+import com.mygdx.eightfold.screens.ScreenInterface;
+
+
+public abstract class GameEntity {
+    protected float x, y, velX, velY, speed;
+    protected float width, height;
+    protected Body body;
+
+    public GameEntity(float width, float height, Body body, ScreenInterface screenInterface, GameAssets gameAssets) {
+        this.x = body.getPosition().x;
+        this.y = body.getPosition().y;
+        this.width = width;
+        this.height = height;
+        this.body = body;
+        this.velX = 0;
+        this.velY = 0;
+        this.speed = 0;
+    }
+
+    public abstract void update(float delta);
+
+    public abstract void render(SpriteBatch batch);
+
+    public Body getBody() {
+        return body;
+    }
+}
